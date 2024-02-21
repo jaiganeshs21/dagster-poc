6fdf93ab4e6485496439f67cb2308f7df69fc35f
Jai <jaiganeshs2105@gmail.com>
2024-02-21 17:19:28 +0530

    refactor

--- my_dagster_project/everstage_api_task.py
+++ my_dagster_project/everstage_api_task.py
@@ -1,11 +1,13 @@
+import json
 from everstage_api_wrapper import EverstageAPIWrapper
 
 
-import json
 
-
-def everstage_api_task():
-    url = "http://localhost:8000/airflow/connector/get-datasheet-names"
+def submit_task_to_everstage():
+    """
+    This function submits a task to Everstage and returns the job_id
+    """
+    url = "http://localhost:8000/airflow/submit-task"
     email = "test@datasheet.graph"
     client_id = 3001
 
@@ -15,9 +17,35 @@ def everstage_api_task():
     response = api_invoker.session.get(url, data=payload)
 
     if response.ok:
-        print("RECEIVED TABLE NAMES FROM EVERSTAGE API")
-        print("response", response)
-        print("response.json()", json.dumps(response.json(), indent=4))
+        print("AIRFLOW: submitted task to Everstage")
+        response_json = response.json()
+        return response_json.get('job_id')
     else:
-        print("FAILED TO RECEIVE TABLE NAMES FROM EVERSTAGE API")
-        print("response", response)
+        print("AIRFLOW: Failed to submit task to Everstage")
+        
+        
+        
+def everstage_task_status_checker():
+    """
+    This function checks the status of a task in Everstage and returns the status
+    """
+    base_url = "http://localhost:8000/airflow/status-checker"
+    email = "test@datasheet.graph"
+    client_id = 3001
+
+    # Add job_id as a query parameter to the URL
+    url = f"{base_url}?job_id={123}"
+
+    payload = json.dumps({"email": email, "client_id": client_id})
+
+    api_invoker = EverstageAPIWrapper()
+    response = api_invoker.session.get(url, data=payload)
+
+    if response.ok:
+        response_json = response.json()
+        print("AIRFLOW: received task status from Everstage", response_json.get('status'))  
+        return response_json.get('status')
+    else:
+        print("AIRFLOW: Failed to submit task to Everstage")
+        
+        
--- my_dagster_project/external_api_calls.py
+++ my_dagster_project/external_api_calls.py
@@ -1,21 +1,23 @@
 from dagster import op, sensor, RunRequest, job
-from everstage_api_task import everstage_api_task
+from everstage_api_task import submit_task_to_everstage, everstage_task_status_checker
+
 
 @op
-def start_long_running_job():
-    print("DAGSTER: inside start_long_running_job()")
-    everstage_api_task()
+def submit_task():
+    print("AIRFLOW: inside submit_task function")
+    job_id = submit_task_to_everstage()
+    print("AIRFLOW: job_id", job_id)
+
+@sensor(job_name="process_job_results")
+def sensor_task():
+    print("AIRFLOW: inside sensor_task function")
+    job_status = everstage_task_status_checker()
+    if job_status == "FAILED":
+        print("AIRFLOW: Job failed")
+        return
+    # yield RunRequest(run_key=None)
 
 @job
 def my_pipeline():
-    job_id = start_long_running_job()
-    print("Job ID: ", job_id)
-
-
-# @sensor(job_name="process_job_results")
-# def long_running_job_sensor(context):
-#     job_status = external_service.check_job_status()  # Check the status of the job
-#     if job_status == "COMPLETED":
-#         yield RunRequest(run_key="unique_run_key", run_config={})
-#     elif job_status == "FAILED":
-#         context.log.error("Long-running job failed.")
+    submit_task()
+    sensor_task()
